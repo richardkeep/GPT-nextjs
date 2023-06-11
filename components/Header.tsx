2b3b902578b8f3f54a795b5cd3858d87552a9d23
richardkeep <r.kipsang@gmail.com>
2023-06-11 20:47:53 +0300

    wip

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,17 +3,17 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
+    <header className="flex items-center justify-between w-full px-2 mt-5 border-b-2 pb-7 sm:px-4">
       <Link href="/" className="flex space-x-3">
         <Image
           alt="header text"
           src="/writingIcon.png"
-          className="sm:w-12 sm:h-12 w-8 h-8"
+          className="w-8 h-8 sm:w-12 sm:h-12"
           width={32}
           height={32}
         />
-        <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">
-          twitterbio.io
+        <h1 className="ml-2 text-2xl font-bold tracking-tight sm:text-4xl">
+          academia.io
         </h1>
       </Link>
       <a
